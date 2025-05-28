4ee115e670c0c66a411bf18f167d25ccbf20dfec
omkumar20 <kumarom2000.ok@gmail.com>
2025-05-28 07:33:11 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// 👉 Replace 'ngo-website' with your actual GitHub repo name
 export default defineConfig({
-  plugins: [  tailwindcss(),react()],
+  base: '/ngo-website/',
+  plugins: [react(), tailwindcss()]
 })
